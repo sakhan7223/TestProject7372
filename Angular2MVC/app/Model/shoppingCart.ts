26a6811c9cf3527f8317e84bb923bc1bd1c39043
sakhan7223 <33336344+sakhan7223@users.noreply.github.com>
2017-11-12 15:59:10 -0600

    added checkbox and shoppingcart

---
+++ Angular2MVC/app/Model/shoppingCart.ts
@@ -0,0 +1,8 @@
+﻿import { userItem } from '../Model/userItem';
+
+export class shoppingCart { //its a class not interface
+    Id: number;
+    userId: number;
+    userItemList: userItem[];
+   
+}
